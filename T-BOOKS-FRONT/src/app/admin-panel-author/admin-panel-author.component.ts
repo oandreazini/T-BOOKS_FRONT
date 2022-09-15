bed368b1c2bb2aa8dfa028b2c5789da240ae351c
Bárbara Broto <barbara.broto97@gmail.com>
2022-09-15 18:33:13 +0200

    update admin panel authors

--- T-BOOKS-FRONT/src/app/admin-panel-author/admin-panel-author.component.ts
+++ T-BOOKS-FRONT/src/app/admin-panel-author/admin-panel-author.component.ts
@@ -1,25 +1,36 @@
 import { Component, OnInit,AfterViewInit,ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
+import { IAuthors } from '../models/iauthors';
+import { AuthorsService } from '../services/authors.service';
 
 @Component({
   selector: 'app-admin-panel-author',
   templateUrl: './admin-panel-author.component.html',
   styleUrls: ['./admin-panel-author.component.css']
 })
-export class AdminPanelAuthorComponent implements OnInit, AfterViewInit {
+export class AdminPanelAuthorComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name','action'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  ELEMENT_DATA!: IAuthors[];
+  displayedColumns: string[] = ['nameSurname','action'];
+  dataSource = new MatTableDataSource<IAuthors>(this.ELEMENT_DATA);
+
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
-  constructor() { }
+
+  constructor(private authorService: AuthorsService) { }
 
   ngOnInit(): void {
+    this.getAllAuthors();
+  }
+
+  getAllAuthors(){
+    let resp = this.authorService.returnAllAuthors();
+    resp.subscribe(report => this.dataSource.data = report as IAuthors[]);
   }
 
 }
