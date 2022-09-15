85fc8de6b670ee6252aa64a50e20e372dc4a2abb
Bárbara Broto <barbara.broto97@gmail.com>
2022-09-15 18:59:26 +0200

    update admin panel loans

--- T-BOOKS-FRONT/src/app/models/iloans.ts
+++ T-BOOKS-FRONT/src/app/models/iloans.ts
@@ -1,2 +1,42 @@
 export interface ILoans {
+  id: number;
+  start: string;
+  finish: string;
+  valuation: number;
+  comment: string;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    role: string;
+  };
+  book: {
+    id: number;
+    title: string;
+    isbn: string;
+    synopsis:string;
+    author: {
+      id: number;
+      nameSurname: string;
+    };
+    editorial: {
+      id: number;
+      name: string;
+    };
+    user: {
+      id: number;
+      name: string;
+      email: string;
+      phone: string;
+      username: string;
+      password: string;
+      role: string;
+    };
+  }
+
+
+
 }
