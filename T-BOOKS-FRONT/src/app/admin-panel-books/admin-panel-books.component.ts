becb4d619e792a58b2a76301541d0c3307b6d2c7
Bárbara Broto <barbara.broto97@gmail.com>
2022-09-15 12:48:42 +0200

    add admin panel books v3

--- T-BOOKS-FRONT/src/app/admin-panel-books/admin-panel-books.component.ts
+++ T-BOOKS-FRONT/src/app/admin-panel-books/admin-panel-books.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit,AfterViewInit,ViewChild } from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-// import { BooksService } from '../books.service';
-// import { IBooks } from '../models/ibooks';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-panel-books',
@@ -12,29 +7,9 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class AdminPanelBooksComponent implements OnInit {
 
-  // ELEMENT_DATA!: IBooks[];
-  // displayedColumns: string[] = ['title', 'isbn','synopsis','action'];
-  // dataSource = new MatTableDataSource<IBooks>(this.ELEMENT_DATA);
-
-
-  @ViewChild(MatPaginator)
-  paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
-  }
-
-  // private booksService: BooksService
   constructor() { }
 
   ngOnInit(): void {
-    this.getAllBooks();
-  }
-
-  getAllBooks(){
-    // let resp = this.booksService.returnAllBooks();
-    // resp.subscribe(report=> this.dataSource.data = report as IBooks[]);
-
   }
 
 }
