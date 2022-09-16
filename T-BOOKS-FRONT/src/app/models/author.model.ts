aa1ac03977dc8b53c7363010eaf4d4311cad53e8
oandreazini <oriol_as_92@hotmail.com>
2022-09-16 19:01:25 +0200

    Author Model & Service finished

---
+++ T-BOOKS-FRONT/src/app/models/author.model.ts
@@ -0,0 +1,4 @@
+export class Author {
+  id?: any;
+  name_surname?: any;
+}
--- T-BOOKS-FRONT/src/app/services/authors.service.ts
+++ T-BOOKS-FRONT/src/app/services/authors.service.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IAuthors } from '../models/iauthors';
+import { Author } from '../models/author.model';
+
+const baseURL = 'https://rts-tbooks-bbdd.herokuapp.com/authors';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +13,24 @@ export class AuthorsService {
 
   constructor(private http: HttpClient) { }
 
-  returnAllAuthors(){
-    return this.http.get("https://rts-tbooks-bbdd.herokuapp.com/authors");
+  returnAllAuthors(): Observable<Author[]>{
+    return this.http.get<IAuthors[]>(baseURL);
   }
+
+  getById(id: any): Observable<Author>{
+    return this.http.get(`${baseURL}/${id}`);
+  }
+
+  create(data: any): Observable<any>{
+    return this.http.post(baseURL,data);
+  }
+
+  update(id: any, data: any): Observable<any>{
+    return this.http.put(`${baseURL}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any>{
+    return this.http.delete(`${baseURL}/${id}`);
+  }
+
 }
