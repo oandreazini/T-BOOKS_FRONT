4f3ebcd5b7ded9ebb6e93618789ca661396c2d3f
oandreazini <oriol_as_92@hotmail.com>
2022-09-16 20:35:56 +0200

    update mod-loan component

--- T-BOOKS-FRONT/src/app/mod-loan/mod-loan.component.ts
+++ T-BOOKS-FRONT/src/app/mod-loan/mod-loan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Loan } from '../models/loan.model';
+import { LoansService } from '../services/loans.service';
 
 @Component({
   selector: 'app-mod-loan',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mod-loan.component.css']
 })
 export class ModLoanComponent implements OnInit {
+  loan: Loan = {
+    start: '',
+    finish: '',
+    valuation: '',
+    comment: ''
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private loanService: LoansService) { }
 
   ngOnInit(): void {
+  }
+
+  saveLoan(): void {
+    const data = {
+      start: this.loan.start,
+      finish: this.loan.finish,
+      valuation: this.loan.valuation,
+      comment: this.loan.comment,
+    };
+
+    this.loanService.create(data).subscribe(
+      response => {
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {console.log(error)}
+    )
   }
 
 }
